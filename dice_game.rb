9d013aa854a6f5406a28d319a67176cd6df46211
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-09 10:44:55 +0000

    changed exit code to prompt loop

--- dice_game.rb
+++ dice_game.rb
@@ -3,36 +3,40 @@ player_2 = 0
 
 until player_1 == 2 || player_2 == 2 do
     puts "Player 1: type 'roll'"
-    input = gets.chomp
     
-        if input == 'roll'
+    while input = gets.chomp
+        case input
+        when 'roll'
             roll_1 = rand(1..6)
             puts roll_1
+            break
         else
             puts "Please type 'roll'"
-            exit 1
         end
+    end
         
-        puts "Player 2: type 'roll'"
-        input = gets.chomp
-        
-        if input == 'roll'
+    puts "Player 2: type 'roll'"
+    
+    while input = gets.chomp
+        case input
+        when 'roll'
             roll_2 = rand(1..6)
             puts roll_2
+            break
         else
             puts "Please type 'roll'"
-            exit 1
         end
+    end
     
-        if roll_1 > roll_2
-            puts "Player 1 wins the round!"
-            player_1 += 1
-        elsif roll_2 > roll_1
-            puts "Player 2 wins the round!"
-            player_2 += 1
-        else
-            puts "Draw!"
-        end
+    if roll_1 > roll_2
+        puts "Player 1 wins the round!"
+        player_1 += 1
+    elsif roll_2 > roll_1
+        puts "Player 2 wins the round!"
+        player_2 += 1
+    else
+        puts "Draw!"
+    end
 end
 
 if player_1 > player_2
@@ -40,3 +44,9 @@ if player_1 > player_2
 else
     puts "Player 2 wins!"
 end
+
+
+
+    
+
+
